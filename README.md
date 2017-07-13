95e52bafe98124734e1c96d1b3562d0f2f2e76c8
cqggyy <cqcar@outlook.com>
2017-07-12 22:40:23 -0400

    update hello & readme
    update readme
    update hello.py, can running

--- README.md
+++ README.md
@@ -5,5 +5,9 @@ Flask的学习代码
 
 python的所有命令：
 1.激活VENV的虚拟环境
-script/activate
-script/deactivate
+enve\scripts\activate
+enve\scripts\deactivate
+
+安装Flask：
+pip install flask
+
--- hello.py
+++ hello.py
@@ -1,3 +1,9 @@
-import flask from Flask
+from flask import Flask
+app = Flask(__name__)
 
-manage(app)
+@app.route('/')
+def index():
+    return '<h1>Hello World!</h1>'
+
+if __name__ == '__main__':
+    app.run(debug=True)
