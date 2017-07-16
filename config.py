3667f0b6d29b5361ee0e730c768c9435d7e26f8f
cqggyy <cqcar@outlook.com>
2017-07-16 00:38:39 -0400

    update

--- config.py
+++ config.py
@@ -1,42 +1,46 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-class Config:
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
-	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-	FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-	FLASKY_MAIL_SENDER = 'Flasky Admin<admin@autofix.com>'
-	FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
-	@staticmethod
-	def init_app(app):
-                pass
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
 
 class DevelopmentConfig(Config):
-	"""docstring for DevelopmentConfig"""
-	DEBUG = True
-	MAIL_SERVER = 'smtp.google.com'
-	MAIL_PORT = 587
-	MAIL_USE_TLS = True
-	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-				'mysql+pymysql://ggyy:samsung@192.168.1.240/flask'
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'mysql+pymysql://ggyy:samsung@192.168.1.240/flask' 
+
 
 class TestingConfig(Config):
-	"""docstring for DevelopmentConfig"""
-	TESTING = True
-	SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-				'mysql+pymysql://ggyy:samsung@192.168.1.240/flask'
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'mysql+pymysql://ggyy:samsung@192.168.1.240/flask' 
+
 
 class ProductionConfig(Config):
-	"""docstring for DevelopmentConfig"""
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-				'mysql+pymysql://ggyy:samsung@192.168.1.240/flask'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'mysql+pymysql://ggyy:samsung@192.168.1.240/flask'
+
 
 config = {
-	'development': DevelopmentConfig,
-	'testing': TestingConfig,
-	'production': ProductionConfig,
-	'default': DevelopmentConfig
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
 }
